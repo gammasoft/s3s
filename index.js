1b370031c376fa521b2b60675cd2d7c968e8be9a
Renato Gama <renatoargh@gmail.com>
2014-10-23 19:15:53 -0200

    First version

---
+++ index.js
@@ -0,0 +1,150 @@
+'use strict';
+
+var utils = require('gammautils'),
+    moment = require('moment-timezone'),
+    mime = require('mime'),
+
+    forEachOwnProperty = utils.object.forEachOwnProperty,
+    hmac = utils.crypto.hmac;
+
+function formatGmtTime(date) {
+    if(typeof date === 'string') {
+        date = new Date(date);
+    }
+
+    return new moment(date).tz('GMT').format('ddd[,] DD MMM YYYY HH:mm:ss ZZ');
+}
+
+function canonicalizeAmazonHeaders(amazonHeaders) {
+    var headers = [];
+
+    forEachOwnProperty(amazonHeaders, function(key, values) {
+        if(!Array.isArray(values)) {
+            values = [values];
+        }
+
+        key = key.toLowerCase().trim();
+
+        if(key.indexOf('x-amz-') !== 0) {
+            key = 'x-amz-' + key;
+        }
+
+        headers.push(key + ':' + values.map(function(value) {
+            return value.trim();
+        }).join(','));
+    });
+
+    return headers.sort().join('\n');
+}
+
+function canonicalizeResource(bucket, key) {
+    var resource = '/' + bucket + '/' + key;
+
+    if(resource === '//') {
+        resource = '/';
+    }
+
+    return resource;
+}
+
+function addMinutes(date, minutes) {
+    return new Date(date.getTime() + (minutes * 60 * 1000));
+}
+
+function formatUNIX(date) {
+    if(typeof date === 'number') {
+        return date;
+    }
+
+    return Math.round(date.getTime() / 1000);
+}
+
+module.exports = (function() {
+    function S3S(options) {
+        this.options = options;
+    }
+
+    S3S.prototype.getCredentials = function(args) {
+        args.debug = (args.debug && args.debug.toString() === 'true') || this.options.debug;
+        args.accessKeyId = args.accessKeyId || this.options.accessKeyId;
+        args.secretAccessKey = args.secretAccessKey || this.options.secretAccessKey;
+        args.key = args.key || '';
+        args.method = args.method || 'GET';
+        args.bucket = args.bucket || this.options.bucket || '';
+        args.contentType = args.contentType || (args.key && mime.lookup(args.key));
+        args.amazonHeaders = args.amazonHeaders || {};
+        args.date = (args.date && new Date(args.date)) || new Date();
+        args.contentMd5 = args.contentMd5 || '';
+
+        if(args.expires) {
+            args.expires = formatUNIX(args.expires);
+            args.date = args.expires;
+        } else {
+            args.date = formatGmtTime(args.date);
+        }
+
+        if(args.amazonHeaders['x-amz-date']) {
+            args.amazonHeaders['x-amz-date'] = formatGmtTime(args.amazonHeaders['x-amz-date']);
+        }
+
+        function getDate() {
+            if(args.expires) {
+                return args.date;
+            }
+
+            if(args.amazonHeaders['x-amz-date']) {
+                return '';
+                //return args.amazonHeaders['x-amz-date']; //To make example #5 pass
+            }
+
+            return args.date;
+        }
+
+        var canonicalizedAmazonHeaders = canonicalizeAmazonHeaders(args.amazonHeaders),
+            canonicalizedResource = canonicalizeResource(args.bucket, args.key),
+            notGetNorDelete = ['GET', 'DELETE'].indexOf(args.method) === -1,
+            stringToSign = [
+                args.method,
+                notGetNorDelete ? args.contentMd5 : '',
+                notGetNorDelete ? args.contentType : '',
+                getDate()
+            ];
+
+        if(notGetNorDelete && canonicalizedAmazonHeaders) {
+            stringToSign.push(canonicalizedAmazonHeaders);
+        }
+
+        stringToSign.push(canonicalizedResource);
+        stringToSign = stringToSign.join('\n');
+
+        if(args.debug === true) {
+            process.stdout.write(stringToSign);
+        }
+
+        return {
+            awsAccessKey: args.accessKeyId,
+            date: args.expires || args.date || args.amazonHeaders['x-amz-date'],
+            key: args.key,
+            contentType: args.contentType,
+            signature: hmac('sha1', args.secretAccessKey, stringToSign)
+        };
+    };
+
+    S3S.prototype.getSignedUrl = function(args) {
+        args.expires = args.expires || addMinutes(new Date(), 5);
+
+        var credentials = this.getCredentials(args);
+
+        return [
+            args.secure === false ? 'http://' : 'https://',
+            args.bucket,
+            '.s3.amazonaws.com/',
+            encodeURI(args.key) + '?',
+            'AWSAccessKeyId=' + credentials.awsAccessKey + '&',
+            'Signature=' + encodeURIComponent(credentials.signature) + '&',
+            'Expires=' + credentials.date
+        ].join('');
+    };
+
+    return S3S;
+})();
---
+++ tests/indexTest.js
@@ -0,0 +1,163 @@
+'use strict';
+
+var S3S = require('../index'),
+    s3sAmazonExamples = new S3S({
+        //http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html
+        accessKeyId: 'AKIAIOSFODNN7EXAMPLE',
+        secretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+    });
+
+module.exports = {
+    'First step is to instantiate a new instance': function(test) {
+        var s3s = new S3S({
+            accessKeyId: 'yourAccessKeyId',         //Instantiating with defaults is just a
+            secretAccessKey: 'yourSecretAccessKey', //convenience so you don't have to pass
+            bucket: 'yourBucket'                    //the same parameters all the time.
+        });
+
+        test.notEqual(s3s, null);
+        test.done();
+    },
+
+    'S3S exposes two single functions: **getSignature** and **getSignedUrl**': function(test) {
+        var s3s = new S3S();
+
+        test.equal(typeof s3s.getCredentials, 'function'); //returns a hash
+        test.equal(typeof s3s.getSignedUrl, 'function'); //returns an url
+        test.done();
+    },
+
+    'Obtaining a signed url to get a private object from S3 is very simple': function(test) {
+        var s3s = new S3S({
+                accessKeyId: 'yourAccessKeyId',
+                secretAccessKey: 'yourSecretAccessKey',
+                bucket: 'yourBucket',
+            }),
+
+            signedUrl = s3s.getSignedUrl({
+                key: 'yourFile.png',
+                expires: new Date(2015, 0, 1) //optional - after this date the url expires,
+            });                               //you can omit since defaults to five minutes from
+                                              //current time
+
+        test.equal(typeof signedUrl, 'string');
+
+        test.done();
+    },
+
+    'Amazon Example #1': function(test) {
+        var credentials = s3sAmazonExamples.getCredentials({
+            method: 'GET',
+            key: 'photos/puppy.jpg',
+            date: new Date('Tue, 27 Mar 2007 19:36:42 +0000'),
+            bucket: 'johnsmith'
+        });
+
+        test.equal(credentials.signature, 'bWq2s1WEIj+Ydj0vQ697zp+IXMU=');
+        test.done();
+    },
+
+    'Amazon Example #2': function(test) {
+        var credentials = s3sAmazonExamples.getCredentials({
+            method: 'PUT',
+            key: 'photos/puppy.jpg',
+            date: new Date('Tue, 27 Mar 2007 21:15:45 +0000'),
+            bucket: 'johnsmith'
+        });
+
+        test.equal(credentials.signature, 'MyyxeRY7whkBe+bq8fHCL/2kKUg=');
+        test.done();
+    },
+
+    'Amazon Example #3': function(test) {
+        var credentials = s3sAmazonExamples.getCredentials({
+            method: 'GET',
+            key: '',
+            date: new Date('Tue, 27 Mar 2007 19:42:41 +0000'),
+            bucket: 'johnsmith'
+        });
+
+        test.equal(credentials.signature, 'htDYFYduRNen8P9ZfE/s9SuKy0U=');
+        test.done();
+    },
+
+    'Amazon Example #4': function(test) {
+        var credentials = s3sAmazonExamples.getCredentials({
+            method: 'GET',
+            key: '?acl',
+            date: new Date('Tue, 27 Mar 2007 19:44:46 +0000'),
+            bucket: 'johnsmith'
+        });
+
+        test.equal(credentials.signature, 'c2WLPFtWHVgbEmeEG93a4cG37dM=');
+        test.done();
+    },
+
+    'Amazon Example #5 (broken example from AWS)': function(test) {
+        // var credentials = s3sAmazonExamples.getCredentials({
+        //     method: 'DELETE',
+        //     date: new Date('Tue, 27 Mar 2007 21:20:26 +0000'),
+        //     bucket: 'johnsmith',
+        //     key: 'photos/puppy.jpg',
+        //     amazonHeaders: {
+        //         'x-amz-date': new Date('Tue, 27 Mar 2007 21:20:26 +0000')
+        //     }
+        // });
+
+        // test.equal(credentials.signature, 'lx3byBScXR6KzyMaifNkardMwNk=');
+        test.done();
+    },
+
+    'Amazon Example #6': function(test) {
+        var credentials = s3sAmazonExamples.getCredentials({
+            method: 'PUT',
+            date: new Date('Tue, 27 Mar 2007 21:06:08 +0000'),
+            bucket: 'static.johnsmith.net',
+            key: 'db-backup.dat.gz',
+            contentType: 'application/x-download',
+            contentMd5: '4gJE4saaMU4BqNR0kLY+lw==',
+            amazonHeaders: {
+                'x-amz-acl': 'public-read',
+                'X-Amz-Meta-ReviewedBy': ['joe@johnsmith.net', 'jane@johnsmith.net'],
+                'X-Amz-Meta-FileChecksum': '0x02661779',
+                'X-Amz-Meta-ChecksumAlgorithm': 'crc32'
+            }
+        });
+
+        test.equal(credentials.signature, 'ilyl83RwaSoYIEdixDQcA4OnAnc=');
+        test.done();
+    },
+
+    'Amazon Example #7': function(test) {
+        var credentials = s3sAmazonExamples.getCredentials({
+            method: 'GET',
+            date: new Date('Wed, 28 Mar 2007 01:29:59 +0000')
+        });
+
+        test.equal(credentials.signature, 'qGdzdERIC03wnaRNKh6OqZehG9s=');
+        test.done();
+    },
+
+    'Amazon Example #8': function(test) {
+        var credentials = s3sAmazonExamples.getCredentials({
+            method: 'GET',
+            key: 'fran%C3%A7ais/pr%c3%a9f%c3%a8re',
+            bucket: 'dictionary',
+            date: new Date('Wed, 28 Mar 2007 01:49:49 +0000')
+        });
+
+        test.equal(credentials.signature, 'DNEZGsoieTZ92F3bUfSPQcbGmlM=');
+        test.done();
+    },
+
+    'Amazon Example #9': function(test) {
+        var url = s3sAmazonExamples.getSignedUrl({
+            bucket: 'johnsmith',
+            key: 'photos/puppy.jpg',
+            expires: new Date(1175139620 * 1000)
+        });
+
+        test.equal(url, 'https://johnsmith.s3.amazonaws.com/photos/puppy.jpg?AWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&Signature=NpgCjnDzrM%2BWFzoENXmpNDUsSn8%3D&Expires=1175139620');
+        test.done();
+    }
+};
